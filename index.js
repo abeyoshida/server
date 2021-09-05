da0a9a37313e76ddfe378a46c364881d297439df
Abe Yoshida <abeyoshida@gmail.com>
2021-09-05 11:05:10 -0400

    Add comment on authRoutes

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ require("./services/passport");
 /** Express server */
 const app = express();
 
+/**
+ * authRoutes returns a function which we immediately call with app as an argument.
+ */
 require("./routes/authRoutes")(app);
 
 /** Express tells Node to listen to port 5000 for any incoming traffic. */
